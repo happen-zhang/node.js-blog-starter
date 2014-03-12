ea75964b0ad38ddd14953c671447d76aabe930e2
happen-zhang <zhanghaipeng404@gmail.com>
2014-03-12 19:37:48 +0800

    修改mocha初始化文件

--- test/_init.js
+++ test/_init.js
@@ -2,22 +2,8 @@
  * mocha initilize file
  */
 
-var mongoose = require('mongoose');
-var util = require('../libs/util');
-
-// test env
 process.env.NODE_ENV = 'test';
 
 var chai = require('chai');
-
-// connect mongodb
-var dbConfig = {
-  host: 'localhost',
-  port: 27017,
-  name: 'blog_test',
-  username: '',
-  password: ''
-};
-mongoose.connect(util.generateMongoUrl(dbConfig));
 
 global.should = chai.should();
